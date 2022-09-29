ca022f47e0df8b684e7b002fab5e7a9a527d180a
nokcharathae <rkwmal0000@naver.com>
2022-09-29 18:31:46 +0900

    up

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,16 @@
-/* Hello World in NodeJS */
-console.log("Hello, World!");
+var PORT = 3002;
+var HOST = '127.0.0.1';
+
+var dgram = require('dgram');
+var message = new Buffer('Hellow world...');
+
+
+var client = dgram.createSocket('udp4');
+
+client.send(message, 0, message.length, PORT, HOST, function(err, bytes){
+    if (err) throw err;
+    
+        console.log('UDP message sent to ' + HOST + ':'+PORT);
+        client.close(); 
+   
+})
